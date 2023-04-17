f23033061d8fab0a0db75c52122755594663b03c
SinclairPythonAkoto <sinclairakoto@gmail.com>
2023-04-17 02:45:05 +0100

    email library page added

--- aurous79/app.py
+++ aurous79/app.py
@@ -18,7 +18,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 @app.route("/")
 def home():
-    return render_template("single_email.html")
+    return render_template("email_library.html")
 
 @app.route("/feedback")
 def feedback():
